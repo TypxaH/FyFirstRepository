c35c52dab61d4074a0b11f03efaae1075f4f1ab5
TypxaH <turhan@abv.bg>
2023-01-27 12:11:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phones = new Dictionary<string, string>();
+            string info = Console.ReadLine();
+            while (info != "END")
+            {
+
+            }
+        }
+    }
+}
